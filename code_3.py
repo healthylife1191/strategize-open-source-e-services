feea650fb3b0837db045737fb894b8c97e74d0c2
healthylife1191 <159755852+healthylife1191@users.noreply.github.com>
2024-07-18 11:31:23 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+function generateRandomData() {
+    return data;
+
+    data.forEach(item => {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+function main() {
+        console.log("Random Number: " + item);
+main();
+
+    }
+    });
+
+    const data = [];
